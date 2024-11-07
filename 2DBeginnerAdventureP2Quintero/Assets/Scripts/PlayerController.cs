333ce17cc9c95b3ed791e419a8dc64a015d992a4
David Quintero <dquintero@dusd.net>
2024-11-07 09:58:54 -0800

    health Collectable
    created

--- 2DBeginnerAdventureP2Quintero/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP2Quintero/Assets/Scripts/PlayerController.cs
@@ -1,27 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 4.0f;
 
+    public int macHealth = 5;
+    public float timeInvincible = 2;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+    Rigidbody2D rigidbody2d;
+
+    float horizontal = 3.0f;
+    float vertical = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+       rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
